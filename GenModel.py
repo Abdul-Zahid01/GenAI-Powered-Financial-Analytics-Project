daf1cb49d4cfb503513e5e2f8593f6d35144509a
Abdul-Zahid01 <96014125+Abdul-Zahid01@users.noreply.github.com>
2025-11-26 05:25:21 -0800

    Update GenModel.py

--- GenModel.py
+++ GenModel.py
@@ -3,9 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("AIzaSyD4tMh5rf_Hl02hev1qmzZwR8CDqlwiPC0"))
 
 print("Listing available models...")
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
+
         print(m.name)
